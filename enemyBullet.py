06d259cd887a8f3e1298da166ab570c2cc7eae4b
Stephanie Margaret Tilden <stilden@student00.cse.nd.edu>
2014-05-06 17:56:01 -0400

    STarted enemy bullet clas

---
+++ enemyBullet.py
@@ -0,0 +1,32 @@
+# Nathaniel Pawelczyk & Stephanie Tilden
+# CSE 30332
+# Pygame + Twisted Final Project
+# Due May 7, 2014
+
+# bullet.py provides bullet functionality
+
+import pygame
+import math
+
+class enemyBullet(pygame.sprite.Sprite):
+	def __init__(self, gs, angle=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		# initialize bullet info
+		self.rect = gs.bulletImage.get_rect()
+		# start bullet behind the player at the center
+		self.rect.center = gs.player.rect.center
+
+		self.angle = angle
+		self.remove = False
+
+		# find horizontal and vertical speed according to the angle
+		self.hspeed = math.cos(self.angle)
+		self.vspeed = math.sin(self.angle)
+		
+		self.hspeed = self.hspeed*10
+		self.vspeed = -self.vspeed*10
+		
+	def tick(self):
+		# on tick, move the bullet hspeed and vspeed
+		self.rect = self.rect.move(self.hspeed, self.vspeed)
